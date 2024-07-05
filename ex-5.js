be558c14e3ed778a9cc0c77244d0ad21401a9e79
konthron gongmanee <konthron.ant@gmail.com>
2024-07-06 00:28:54 +0700

    ES6 finish

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,8 @@
-function sum() {
+function sum(...input) {
   // Start coding here !
+  return input.reduce((accumulator,currentValue) => {
+    return accumulator + currentValue;
+  });
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
@@ -7,3 +10,5 @@ let result2 = sum(1, 2, 3, 4);
 
 console.log(`Result ${result1}`);
 console.log(`Result ${result2}`);
+
+
